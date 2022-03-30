6caeb0b1e8d7022f7712c37889567d492d9957c2
unknown <adrien.gottardi@outlook.fr>
2022-03-30 15:18:19 +0200

    initial commit

---
+++ NFT/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.http import *
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, "index.html", context)
